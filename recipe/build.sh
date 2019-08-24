e9e0b36b8a4f171f3520f6e286a3099f8add19f9
Simon Bray <32272674+simonbray@users.noreply.github.com>
2019-08-24 13:54:51 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,12 @@
-echo 'N' | ./configure  -noX11 -norism --with-python ${PREFIX}/bin/python --python-install local gnu
+if [ $(uname) == "Darwin" ]; then
+    export COMPILER_SET="clang"
+fi
+
+if [ $(uname) == "Linux" ]; then
+    export COMPILER_SET="gnu"
+fi
+
+echo 'N' | ./configure  -noX11 -norism --with-python ${PREFIX}/bin/python --python-install local $COMPILER_SET
 # using the -openmp flag causes packages not to be included in the build
 # however, the RISM model requires OpenMP, so -norism is set
 # the --prefix tag does not work, so copy the files manually to $PREFIX
