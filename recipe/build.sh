9673ba6d0620f2b1e80f8567ae24acdaf7d55bf8
Simon Bray <sbray1371@gmail.com>
2019-12-18 19:06:03 +0100

    update tarball link

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,17 @@ if [ $(uname) == "Linux" ]; then
     export COMPILER_SET="gnu"
 fi
 
+tar -xf AmberTools19.tar.bz2.cmlqap
+cd amber18
+
 # Upgrade AmberTools source to the patch level specified by the MINOR version in $PKG_VERSION
 for n in {1..5}; do # try up to five times before failing
     export PATCH_LEVEL=$(echo $PKG_VERSION | cut -d. -f2)
     echo "Upgrading source to patch level $PATCH_LEVEL"
     ./update_amber --update-to=AmberTools.${PATCH_LEVEL} && break
 done
+
+patch -i $RECIPE_DIR/configure.patch -p 2
 
 # Build AmberTools without further patching
 echo 'N' | ./configure  -noX11 -norism --with-python ${PREFIX}/bin/python --python-install local $COMPILER_SET
