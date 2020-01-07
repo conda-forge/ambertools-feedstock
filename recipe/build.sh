321e67b26e0e06e247d7eeb48fcb5beceb6ebcac
Jaime RGP <jaime.rogue@gmail.com>
2020-01-07 11:03:54 +0100

    Patch cmake after AmberTools updates

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,16 @@
 set -euxo pipefail
 
-# Patch manually to avoid issues with newlines
-sed 's/\r$//g' ${RECIPE_DIR}/patches/amber19-fix-cmake.patch > amber19-fix-cmake.patch
-sed -i 's/\r$//g' ${SRC_DIR}/cmake/*.cmake
-patch -p1 --ignore-whitespace -t -i amber19-fix-cmake.patch || true
-
 # Upgrade AmberTools source to the patch level specified by the MINOR version in $PKG_VERSION
 for n in {1..5}; do
     export PATCH_LEVEL=$(echo $PKG_VERSION | cut -d. -f2)
     echo "Upgrading source to patch level $PATCH_LEVEL"
     ./update_amber --update-to=AmberTools.${PATCH_LEVEL} && break
 done
+
+# Patch manually to avoid issues with newlines
+sed 's/\r$//g' ${RECIPE_DIR}/patches/amber19-fix-cmake.patch > amber19-fix-cmake.patch
+sed -i 's/\r$//g' ${SRC_DIR}/cmake/*.cmake
+patch -p1 --ignore-whitespace -t -i amber19-fix-cmake.patch || true
 
 # Build AmberTools with cmake
 mkdir build
@@ -31,6 +31,3 @@ cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/ambertools.sh
 cp ${RECIPE_DIR}/activate.fish ${PREFIX}/etc/conda/activate.d/ambertools.fish
 cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/ambertools.sh
 cp ${RECIPE_DIR}/deactivate.fish ${PREFIX}/etc/conda/deactivate.d/ambertools.fish
-
-# Fix missing file
-cp dat/leap/cmd/leaprc.protein.ff12pol ${PREFIX}/dat/leap/cmd/leaprc.protein.ff12pol || true
