85069254f55ca5ac866125a55c3c09826f1c50ab
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-01-09 14:46:49 +0100

    Enable PowerPC; build with cmake procedure (#16)
    * Enable CI for ppc64le
    
    * Update source URLs and build requirements
    
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.5, and conda-forge-pinning 2019.12.18
    
    * llvm-openmp is only needed for MacOS
    
    * Try with boost-cpp
    
    * Bump build number
    
    * Bring in patches from the MacOS PR
    
    * Fix netcdf/fftw errors
    
    * Update requirements
    
    * Try to fix warnings
    
    * Additional patches are needed for ppc64le
    
    * Patch config.h as a workaround for boost/fftw errors
    
    * Fix include paths in memembed
    
    * Provide lapack, blas and arpack through conda packages
    
    * Debug config.h
    
    * Disable arpack compilation
    
    * Create activate/deactivate dirs
    
    * Force copy config.h to ambertools/src
    
    * Build arpack for now
    
    * Match only the start of the line
    
    * Add -L to Xlibs
    
    * Add missing dependencies to reqs/run
    
    * Add some more tests (hopefully it doesn't take very long)
    
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.5, and conda-forge-pinning 2019.12.18
    
    * Add missing . to pushd
    
    * Tests require AMBERHOME var
    
    * Export AMBERHOME first
    
    * Do not run makte tests in this recipe; check locally if needed
    
    * Add libblas to reqs/run
    
    * Try to use --prefix to fix hardcoded path issues in nab and others
    
    * Try with cmake
    
    * Remove openmp and openmpi for now
    
    * Apply cmake patches from amber19
    
    * Patch manually to debug
    
    * Patch patch :)
    
    * Re-add patch to fix newline errors (?)
    
    * Try patching manually again
    
    * Patch in recipe is read-only; create copy
    
    * Try to fix missing file error
    
    * Patch use of /opt/conda
    
    * Patch ff12pol symlink error
    
    * Patch cmake after AmberTools updates
    
    * Use external netcdf/netcdf-fortran and enable xleap
    
    * Enable OSX
    
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.5, and conda-forge-pinning 2020.01.06
    
    * Fix sed for macos
    
    * Fix typo in build.sh
    
    * User perl as dos2unix replacement since it's multiplatform and it's already a build requirement
    
    * Pin ld in MacOS
    
    * Try to set OSX sysroot manually
    
    * Initialize empty CMAKE_FLAGS
    
    * Disable OSX and rerender with conda smithy
    
    Co-authored-by: Automatic conda-forge administrator <pelson.pub+conda-forge@gmail.com>
    Co-authored-by: Simon Bray <32272674+simonbray@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,4 @@
-if [ $(uname) == "Darwin" ]; then
-    export COMPILER_SET="clang"
-fi
-
-if [ $(uname) == "Linux" ]; then
-    export COMPILER_SET="gnu"
-fi
+set -euxo pipefail
 
 # Upgrade AmberTools source to the patch level specified by the MINOR version in $PKG_VERSION
 for n in {1..5}; do
@@ -13,20 +7,34 @@ for n in {1..5}; do
     ./update_amber --update-to=AmberTools.${PATCH_LEVEL} && break
 done
 
-# Build AmberTools without further patching
-echo 'N' | ./configure  -noX11 -norism --with-python ${PREFIX}/bin/python --python-install local $COMPILER_SET
-# using the -openmp flag causes packages not to be included in the build
-# however, the RISM model requires OpenMP, so -norism is set
-# the --prefix tag does not work, so copy the files manually to $PREFIX
+# Patch manually to avoid issues with newlines
+perl -p  -e 's/\r$//g' ${RECIPE_DIR}/patches/amber19-fix-cmake.patch > amber19-fix-cmake.patch
+perl -pi -e 's/\r$//g' ${SRC_DIR}/cmake/*.cmake
+patch -p1 --ignore-whitespace -t -i amber19-fix-cmake.patch || true
 
-bash amber.sh
+CMAKE_FLAGS=""
+if [[ "$target_platform" == osx* ]]; then
+    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+    export FFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${FFLAGS}"
+fi
 
-make
-make install
+# Build AmberTools with cmake
+mkdir -p build
+cd build
+cmake ${SRC_DIR} ${CMAKE_FLAGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCOMPILER=MANUAL \
+    -DPYTHON_EXECUTABLE=${PYTHON} \
+    -DDOWNLOAD_MINICONDA=FALSE \
+    -DBUILD_GUI=TRUE \
+    -DCHECK_UPDATES=FALSE
 
-mkdir $PREFIX/dat
+make && make install
 
-cp -rf bin/* $PREFIX/bin/
-cp -rf dat/* $PREFIX/dat/
-cp -rf lib/* $PREFIX/lib/
-cp -rf include/* $PREFIX/include/
+# Export AMBERHOME automatically
+mkdir -p ${PREFIX}/etc/conda/{activate,deactivate}.d
+cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/ambertools.sh
+cp ${RECIPE_DIR}/activate.fish ${PREFIX}/etc/conda/activate.d/ambertools.fish
+cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/ambertools.sh
+cp ${RECIPE_DIR}/deactivate.fish ${PREFIX}/etc/conda/deactivate.d/ambertools.fish
