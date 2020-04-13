b83e9562f36bf2a40020801bc95057ddc761fa12
Jaime RGP <jaime.rogue@gmail.com>
2020-04-13 21:17:16 +0200

    Add some patches to make rc2 build

---
+++ recipe/all_executables.sh
@@ -0,0 +1,111 @@
+set -euxo pipefail
+
+### Commented commands can't be tested from CLI without input arguments
+# addles -h
+# AddToBox -h
+# cestats --help
+# charmmlipid2amber.py -h
+# ChBox -h
+# cphstats -h
+# elsize
+# gbnsr6 -h
+# gem.pmemd
+# gwh -h
+# hcp_getpdb -h
+# make_crd_hg
+# makeDIST_RST
+# mdout_analyzer.py -h
+# metatwist -h
+# mm_pbsa_nabnmode
+# mm_pbsa_statistics.pl -h
+# mm_pbsa.pl
+# mmpbsa_py_energy
+# mmpbsa_py_nabnmode -h
+# molsurf -h
+# mpinab2c -h
+# nab -h
+# nab2c -h
+# nef_to_RST -h
+# nmode -h
+# OptC4.py -h ## REQUIRES OPENMM
+# packmol
+# paramfit -h
+# parmcal -h
+# parmchk2
+# pbsa
+# process_mdout.perl -h
+# process_minout.perl -h
+# PropPDB -h
+# residuegen -h
+# rism1d -h
+# rism3d.orave -h
+# rism3d.snglpnt -h
+# rism3d.thermo -h
+# sander -h
+# sander.LES -h
+# saxs_rism -h
+# senergy -h
+# simplepbsa
+# xaLeap -h ## REQUIRES DISPLAY
+# xleap -h ## REQUIRES DISPLAY
+# xparmed -h ## REQUIRES DISPLAY
+# XrayPrep ## REQUIRES PHENIX
+add_pdb -h
+add_xray -h
+am1bcc -h
+amb2chm_par.py -h
+amb2chm_psf_crd.py -h
+amb2gro_top_gro.py -h
+ambmask -h
+ambpdb -h
+ante-MMPBSA.py -h
+antechamber -h
+atomtype -h
+bondtype -h
+car_to_files.py -h
+CartHess2FC.py -h
+ceinutil.py -h
+cpeinutil.py -h
+cpinutil.py -h
+cpptraj -h
+draw_membrane2
+espgen -h
+espgen.py -h
+FEW.pl -h
+ffgbsa -h
+finddgref.py -h
+fitpkaeo.py -h
+fixremdcouts.py -h
+genremdinputs.py -h
+IPMach.py -h
+makeANG_RST -help
+MCPB.py -h
+mdgx -h
+mdnab -h
+mdout2pymbar.pl -h
+metalpdb2mol2.py -h
+minab -h
+MMPBSA.py -h
+mol2rtf.py -h
+nfe-umbrella-slice -h
+packmol-memgen -h
+parmed -h
+pdb4amber -h
+PdbSearcher.py -h
+prepgen -h
+ProScrs.py -h
+pymdpbsa -h
+pytleap -h
+reduce -V
+resp -h
+respgen -h
+saxs_md -h
+sgldinfo.sh -h
+sgldwt.sh -h
+softcore_setup.py -h
+sqm -h
+sviol -h
+sviol2 -h
+teLeap -h
+tleap -h
+UnitCell
