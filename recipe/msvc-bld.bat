40be291f2ad849ec538de5b7ad91e8764f96a596
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-05-17 22:03:53 +0200

    add MSVC attempts in separate files for reference

--- recipe/msvc-bld.bat
+++ recipe/msvc-bld.bat
@@ -3,33 +3,32 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`bash -c "echo $PKG_VERSION | cut -d. -f2"`) 
     SET PATCH_LEVEL=%%F || goto :error
 )
 ECHO "Upgrading source to patch level %PATCH_LEVEL%" || goto :error
-python update_amber --update-to=AmberTools.%PATCH_LEVEL% || goto :error
+%PYTHON% update_amber --update-to=AmberTools.%PATCH_LEVEL% || goto :error
 
 :: Additional build dependencies
 copy extra-bc\bin\bc.exe %BUILD_PREFIX%\Library\bin\bc.exe
 copy tcsh.exe %BUILD_PREFIX%\Library\bin\csh.exe
 copy libxblas.a %LIBRARY_PREFIX%\lib\libxblas.a
-copy %LIBRARY_PREFIX%\lib\fftw3.lib %LIBRARY_PREFIX%\lib\fftw3.a
-:: Poor man patch :)
-copy %RECIPE_DIR%\LibraryUtils.cmake.patched %SRC_DIR%\cmake\LibraryUtils.cmake
 
 :: Build AmberTools with cmake
 rmdir build /s /q
 mkdir build || goto :error
 cd build || goto :error
-set "CMAKE_GENERATOR=MinGW Makefiles"
+set "CMAKE_GENERATOR=NMake Makefiles"
 cmake %SRC_DIR% %CMAKE_FLAGS% ^
-    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCOMPILER=AUTO ^
     -DPYTHON_EXECUTABLE=%PYTHON% ^
     -DBUILD_GUI=FALSE ^
     -DCHECK_UPDATES=FALSE ^
     -DTRUST_SYSTEM_LIBS=TRUE ^
+    -DMKL_INTERFACE_LIBRARY=%LIBRARY_LIB%\mkl_intel_lp64_dll.lib ^
+    -DXBLAS_LIBRARY=%LIBRARY_LIB%\libxblas.a ^
     || goto :error
 
-mingw32-make || goto :error
-mingw32-make install || goto :error
+nmake || goto :error
+nmake install || goto :error
 
 :: Export AMBERHOME automatically
 mkdir %PREFIX%/etc/conda/activate.d || goto :error
