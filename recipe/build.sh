2f3cf30f46747283c06a63cf63e420c2d4d62f0c
H. Vetinari <h.vetinari@gmx.com>
2024-08-28 12:07:59 +1100

    rely on `-DCMAKE_CUDA_ARCHITECTURES`

--- recipe/build.sh
+++ recipe/build.sh
@@ -93,6 +93,13 @@ else
     ENABLE_CUDA=TRUE
     # necessary to find cicc on CUDA >=12.0
     export PATH="${PATH}:${BUILD_PREFIX}/nvvm/bin"
+
+    if [[ ${cuda_compiler_version} == 11.8 ]]; then
+        export CMAKE_CUDA_ARCHS="35-real;53-real;62-real;72-real;75-real;80-real;86-real;89"
+    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+        export CMAKE_CUDA_ARCHS="53-real;62-real;72-real;75-real;80-real;86-real;89-real;90"
+    fi
+    export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHS}"
 fi
 cmake ${CMAKE_ARGS} ${SRC_DIR} ${CMAKE_FLAGS} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
