470268b3f92eecca001439efcb8b4d24bbfa249c
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-02-14 11:14:01 -0700

    try adding -arch arm64 to help ld understand what arch we are trying to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -83,8 +83,11 @@ if [[ "${build_platform}" != "${target_platform}" ]]; then
 		echo "editing out mtune native for mtune apple-m1"
 		# This is super hacky -- but now that we built the host tools, we need to change the flag to build for m1
 		find ../ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/-mtune=native/-march=armv8.3-a/g'
+		find ../ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/CXX -stdlib=libc++/CXX -stdlib=libc++ -arch arm64/g'
 		# Add arm64 to the LDFLAGS 
 		export LDFLAGS="$LDFLAGS -arch arm64"
+		# add arm stuff to as many flags as I can think of
+		CMAKE_FLAGS+=" -DCMAKE_OSX_ARCHITECTURES=arm64"
 	fi
 	cd ${BUILD_PREFIX} || exit
 fi
