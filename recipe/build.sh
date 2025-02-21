49fe5247c5c8a38f054ae0722585b9c3b91b4423
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-02-21 14:30:11 -0700

    Ambertools 23 Rebuild (#159)
    * test if we can build 23 still
    
    * MNT: Re-rendered with conda-build 24.3.0, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.07.19.05.24
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.07.19.05.24
    
    * see if things work without installing csh into the env
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.07.19.05.24
    
    * turn off gui build
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.07.19.05.24
    
    * don't skip older cuda builds, lets see if we can patch it
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.10.18.05.55
    
    * build build number
    
    * pull in changes from https://github.com/conda-forge/ambertools-feedstock/pull/148 (thanks @h-vetinari )
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.10.18.05.55
    
    * add cuda 12.6 support
    
    * lets see what happens if we allow newer clang
    
    * that didn't fix the cxx detection issue
    
    * pin cmake to older version that worked in the past
    
    * don't pin cmake and use newest clang
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.12.12.48.03
    
    * see if the patches are doing more harm then good
    
    * still need to remove nab2c
    
    * naive find and replace mtune with march
    
    * didn't end up needed to use FC in host tools, but added for completness
    
    * replace mtune with mcpu
    
    * crazy hack to check PoC
    
    * see if -march=armv8.3-a works for clang and gfortran
    
    * do a verbose build to try and figure out where we are missing an arch flag
    
    * guard if cd fails, but also cd into correct dir before we build the package
    
    * try setting LDFLAGS -arch arm64
    
    * fix LDFLAGS export (not sure why { was treated as literal
    
    * see if any hacks are hurting us here
    
    * try adding -arch arm64 to help ld understand what arch we are trying to build
    
    * set correct arch
    
    * TODO patch cmake/TargetArch.cmake
    
    * try and patch cmake/TargetArch.cmake
    
    * fix patch
    
    * forgot we need to fix with our hack arm64-apple-darwin20.0: error: unsupported argument 'native' to option '-mtune='
    
    * see if telling cmake we are crosscompling helps -- it this doesn't work I have another idea
    
    * see if setting TARGET_TRIPLE is enough
    
    * if these cmake flags don't work we will do plan b
    
    * see if it likes armv8.3-a more than arm64
    
    * it this doesn't work we will add the linker flags by hand
    
    * switch around flags to correct ones
    
    * see if this gets us back to linker error
    
    * yolo
    
    * bump ci
    
    * see if adding the arch flags to setup.py for sander and cpptraj fixes it
    
    * don't reset CC and CXX in setup.py files
    
    * skip cuda builds
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.19.18.36.33
    
    * see if adding mkl to run will fix issues with linux openmpi builds
    
    * check python 3.13 builds
    
    * MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.45.4, and conda-forge-pinning 2025.02.19.18.36.33
    
    * skip python 3.13 builds and add mkl as a run time dep
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,30 +27,30 @@ if (( $(printf "%02d%02d" ${PKG_VERSION//./ }) <= 2205 )); then
 fi
 
 CMAKE_FLAGS=""
-BUILD_GUI="TRUE"
-if [[ "$target_platform" == osx* ]]; then
-    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
-    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
-    # Hack around https://github.com/conda-forge/gfortran_osx-64-feedstock/issues/11
-    # Taken from https://github.com/awvwgk/staged-recipes/tree/dftd4/recipes/dftd4
-    # See contents of fake-bin/cc1 for an explanation
-    export PATH="${PATH}:${RECIPE_DIR}/fake-bin"
-    # BUILD_GUI="FALSE"
-    # Workarounds for issue #20
-    #  In MacOS, `tk` ships some X11 headers that interfere with the X11 libraries
-    #  1) delete clobbered X11 headers (mix of tk and xorg)
-    rm -rf ${PREFIX}/include/X11/{DECkeysym,HPkeysym,Sunkeysym,X,XF86keysym,Xatom,Xfuncproto}.h
-    rm -rf ${PREFIX}/include/X11/{ap_keysym,keysym,keysymdef,Xlib,Xutil,cursorfont}.h
-    #  2) Reinstall Xorg dependencies
-    #     We temporarily disable the (de)activation scripts because they fail otherwise
-    set +u
-    mv ${BUILD_PREFIX}/etc/conda/{activate.d,activate.d.bak}
-    mv ${BUILD_PREFIX}/etc/conda/{deactivate.d,deactivate.d.bak}
-    CONDA_SUBDIR="$target_platform" conda install --yes --no-deps --force-reinstall -p ${PREFIX} xorg-xproto xorg-libx11
-    mv ${BUILD_PREFIX}/etc/conda/{activate.d.bak,activate.d}
-    mv ${BUILD_PREFIX}/etc/conda/{deactivate.d.bak,deactivate.d}
-    set -u
-fi
+BUILD_GUI="FALSE"
+#if [[ "$target_platform" == osx* ]]; then
+#    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+#    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+#    # Hack around https://github.com/conda-forge/gfortran_osx-64-feedstock/issues/11
+#    # Taken from https://github.com/awvwgk/staged-recipes/tree/dftd4/recipes/dftd4
+#    # See contents of fake-bin/cc1 for an explanation
+#    export PATH="${PATH}:${RECIPE_DIR}/fake-bin"
+#    # BUILD_GUI="FALSE"
+#    # Workarounds for issue #20
+#    #  In MacOS, `tk` ships some X11 headers that interfere with the X11 libraries
+#    #  1) delete clobbered X11 headers (mix of tk and xorg)
+#    rm -rf ${PREFIX}/include/X11/{DECkeysym,HPkeysym,Sunkeysym,X,XF86keysym,Xatom,Xfuncproto}.h
+#    rm -rf ${PREFIX}/include/X11/{ap_keysym,keysym,keysymdef,Xlib,Xutil,cursorfont}.h
+#    #  2) Reinstall Xorg dependencies
+#    #     We temporarily disable the (de)activation scripts because they fail otherwise
+#    set +u
+#    mv ${BUILD_PREFIX}/etc/conda/{activate.d,activate.d.bak}
+#    mv ${BUILD_PREFIX}/etc/conda/{deactivate.d,deactivate.d.bak}
+#    CONDA_SUBDIR="$target_platform" conda install --yes --no-deps --force-reinstall -p ${PREFIX} xorg-xproto xorg-libx11
+#    mv ${BUILD_PREFIX}/etc/conda/{activate.d.bak,activate.d}
+#    mv ${BUILD_PREFIX}/etc/conda/{deactivate.d.bak,deactivate.d}
+#    set -u
+#fi
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == 1 && "${CMAKE_CROSSCOMPILING_EMULATOR:-}" == "" ]]; then
     # Assume that netcdf works
@@ -60,34 +60,67 @@ fi
 # We need to build some x86 bins to compile arm64, so we need to save the paths here so we can use them later
 CC_TARGET=${CC}
 CXX_TARGET=${CXX}
+FC_TARGET=${FC}
 
 if [[ "${build_platform}" != "${target_platform}" ]]; then
     # Build host tools first
     mkdir -p ${BUILD_PREFIX}/amber_host_tools
     mkdir -p build_host_tools
-    cd build_host_tools
+    cd build_host_tools || exit
 	CC=${CC_FOR_BUILD} 
 	CXX=${CXX_FOR_BUILD}
+	FC=${FC_FOR_BUILD}
     cmake ${CMAKE_ARGS} ${SRC_DIR} ${CMAKE_FLAGS} \
         -DBUILD_HOST_TOOLS=TRUE \
         -DCOMPILER=MANUAL \
         -DCMAKE_INSTALL_PREFIX="${BUILD_PREFIX}/amber_host_tools"
-	make
-	make install
+	VERBOSE=1 make
+	VERBOSE=1 make install
 
     CMAKE_FLAGS+=" -DUSE_HOST_TOOLS=TRUE"
     CMAKE_FLAGS+=" -DHOST_TOOLS_DIR=${BUILD_PREFIX}/amber_host_tools"
+	if [[ "$target_platform" == "osx-arm64" ]]; then
+		echo "editing out mtune native for mtune apple-m1"
+		# This is super hacky -- but now that we built the host tools, we need to change the flag to build for m1
+		# TODO see if just removing -mtune=native is enough
+		find ../ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/-mtune=native/-march=armv8.3-a/g'
+        find ../ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/-bundle /-bundle -arch arm64 /g'
+		#find ../ \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/CXX -stdlib=libc++/CXX -stdlib=libc++ -arch armv8.3-a/g'
+		# Add arm64 to the LDFLAGS 
+		#export LDFLAGS="$LDFLAGS -arch arm64"
+		# Tell cmake now we want to build osx-arm64
+		CMAKE_FLAGS+=" -DCMAKE_OSX_ARCHITECTURES=arm64"
+	fi
+	cd ${BUILD_PREFIX} || exit
 fi
 
 # Build AmberTools with cmake
 mkdir -p build
-cd build
+cd build || exit
 # Now we go back to the target arch
 CC=${CC_TARGET}
 CXX=${CXX_TARGET}
+FC=${FC_TARGET}
 
 if [ "${mpi}" = "nompi" ]; then ENABLE_MPI=FALSE; else ENABLE_MPI=TRUE; fi
-if [[ "${cuda_compiler_version}" = "None" ]]; then ENABLE_CUDA=FALSE; else ENABLE_CUDA=TRUE; fi
+
+if [[ "${cuda_compiler_version}" = "None" ]]; then
+    ENABLE_CUDA=FALSE
+else
+    ENABLE_CUDA=TRUE
+    # necessary to find cicc on CUDA >=12.0
+    export PATH="${PATH}:${BUILD_PREFIX}/nvvm/bin"
+
+    if [[ ${cuda_compiler_version} == 11.8 ]]; then
+        export CMAKE_CUDA_ARCHS="35-real;53-real;62-real;72-real;75-real;80-real;86-real;89"
+    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+        export CMAKE_CUDA_ARCHS="53-real;62-real;72-real;75-real;80-real;86-real;89-real;90"
+	elif [[ ${cuda_compiler_version} == 12.6 ]]; then
+        export CMAKE_CUDA_ARCHS="53-real;62-real;72-real;75-real;80-real;86-real;89-real;90"
+    fi
+    export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHS}"
+fi
+
 cmake ${CMAKE_ARGS} ${SRC_DIR} ${CMAKE_FLAGS} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCOMPILER=MANUAL \
@@ -99,8 +132,8 @@ cmake ${CMAKE_ARGS} ${SRC_DIR} ${CMAKE_FLAGS} \
     -DCUDA=${ENABLE_CUDA} \
     -DTRUST_SYSTEM_LIBS=TRUE
 
-make
-make install
+VERBOSE=1 make
+VERBOSE=1 make install
 
 
 # Export AMBERHOME automatically
